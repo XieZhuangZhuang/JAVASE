5fdcc254444329856c48b33ceb6ef7e7ad8fce2c
xiezhuangzhuang <xiezhuangzhuang@jpgk.com.cn>
2024-05-22 11:23:59 +0800

    初始化

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @author xiezhuangzhuang
+ * @version 1.0
+ * @title $NAME
+ * @description
+ * @create 2024/5/22 11:21
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
