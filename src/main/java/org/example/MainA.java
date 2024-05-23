5e37757da748650287a680420d357b59fb82fe47
xiezhuangzhuang <120360057@qq.com>
2024-05-23 13:59:36 +0800

    MainA

--- src/main/java/org/example/MainA.java
+++ src/main/java/org/example/MainA.java
@@ -4,5 +4,9 @@ package org.example;/**
  *@author xiezhuangzhuang
  *@version 1.0
  *@create 2024/5/23 13:41
-**/public class MainA {
+**/
+public class MainA {
+    public static void main(String[] args) {
+
+    }
 }
